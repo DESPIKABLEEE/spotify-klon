e36cc3f14ad39383c96631323b57e2711dfa1858
DESPIKABLEEE <grafikaking.gabriel@gmail.com>
2025-11-04 12:28:34 +0100

    napravljen footer s dummy lnikovima

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,13 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        
+        <div className="footer-left">
+          <a href="#">Preview of spotify</a><br />
+          <a href="#">Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</a>
+        </div>
+        <div className="footer-right">
+          <button>Sign up for free</button>
+        </div>
       </div>
     </>
   )
